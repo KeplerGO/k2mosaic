88d1ca9bd8557c91ede857eb001fabc58dfd5d32
Geert Barentsen <geert@barentsen.be>
2016-08-04 15:43:27 -0700

    Add support for Kepler

--- k2mosaic/ui.py
+++ k2mosaic/ui.py
@@ -14,19 +14,37 @@ def k2mosaic_mosaic(tpf_filenames, cadencenumbers=None, output_prefix='', step=1
     """Mosaic a set of TPF files for a set of cadences."""
     # First obtain the campaign, channel, and desired cadence numbers
     with fits.open(tpf_filenames[0]) as first_tpf:
-        campaign = first_tpf[0].header['CAMPAIGN']
+        try:
+            campaign = first_tpf[0].header['CAMPAIGN']
+            is_k2 = True
+        except KeyError:
+            campaign = first_tpf[0].header['QUARTER']
+            is_k2 = False
+        if campaign == '':  # Hack to deal with C9 raw data
+            campaign = 9
         channel = first_tpf[0].header['CHANNEL']
         if cadencenumbers is None:  # Obtain all cadence numbers
-            cadencenumbers = first_tpf[1].data['CADENCENO']
-        elif isinstance(cadencenumbers, int):
-            cadencenumbers = [cadencenumbers]
-    cadences_to_mosaic = cadencenumbers[::step]
+            cadences_to_mosaic = first_tpf[1].data['CADENCENO'][::step]
+        else:
+            cadencerange = [int(r) for r in cadencenumbers.split("..")]
+            cadences_to_mosaic = list(range(cadencerange[0], cadencerange[1], step))
+            if (cadencerange[0] not in first_tpf[1].data['CADENCENO']
+                or cadencerange[-1] not in first_tpf[1].data['CADENCENO']):
+                print('Error: invalid cadence numbers '
+                      '(ensure numbers are in the range {}-{})'.format(
+                                first_tpf[1].data['CADENCENO'][0],
+                                first_tpf[1].data['CADENCENO'][-1]))
+                return
     # Start the mosaicking
     for count, cadenceno in enumerate(cadences_to_mosaic):
-        output_fn = "{}k2mosaic-c{:02d}-ch{:02d}-cad{}.fits".format(
-                    output_prefix, campaign, channel, cadenceno)
+        if is_k2:
+            letter = 'c'
+        else:
+            letter = 'q'
+        output_fn = "{}k2mosaic-{}{:02d}-ch{:02d}-cad{}.fits".format(
+                    output_prefix, letter, campaign, channel, cadenceno)
         click.echo("Writing {} (cadence {}/{})".format(output_fn, count+1, len(cadences_to_mosaic)))
-        mosaic = core.KeplerChannelMosaic(campaign, channel, cadenceno)
+        mosaic = core.KeplerChannelMosaic(campaign=campaign, channel=channel, cadenceno=cadenceno)
         with click.progressbar(tpf_filenames) as bar:
             for tpf in bar:
                 mosaic.add_tpf(tpf)
@@ -58,8 +76,8 @@ def find(campaign, channel, sc):
 
 @k2mosaic.command()
 @click.argument('filelist', type=click.File('r'))
-@click.option('-c', '--cadenceno', type=int, default=None,
-              help='Cadence number (default: all).')
+@click.option('-c', '--cadenceno', type=str, default=None, metavar='cadenceno1..cadenceno2',
+              help='Cadence number range (default: all).')
 @click.option('-s', '--step', type=int, default=1, metavar='<N>',
               help='Only mosaic every Nth cadence (default: 1).')
 def mosaic(filelist, cadenceno, step):
@@ -70,13 +88,17 @@ def mosaic(filelist, cadenceno, step):
 
 @k2mosaic.command()
 @click.argument('filelist', type=click.File('r'))
+@click.option('-e', '--ext', type=int, default=1,
+              help='FITS extension number (default: 1)')
 @click.option('-r', '--rows', type=str, default=None, metavar='row1..row2',
               help='Row range (default: crop to data)')
 @click.option('-c', '--cols', type=str, default=None, metavar='col1..col2',
               help='Column range (default: crop to data)')
+@click.option('--cut', type=str, default=None, metavar='min_cut..max_cut',
+              help='Minimum/maximum cut levels')
 #@click.option('-r', '--cadenceno', type=int, default=None,
 #              help='Cadence number (default: all).')
-def video(filelist, rows, cols, **kwargs):
+def video(filelist, ext, rows, cols, cut, **kwargs):
     """Turn mosaics into a video or gif.
 
     FILELIST should be a text file specifying the mosaic files to include,
@@ -91,20 +113,23 @@ def video(filelist, rows, cols, **kwargs):
     elif rows.strip() == 'all':
         rowrange = (0, KEPLER_CHANNEL_SHAPE[0])
     else:
-        rowrange = rows.split("..")
+        rowrange = [int(r) for r in rows.split("..")]
 
     if cols is None:
         colrange = (np.min(idx_not_nan[:, 1]), np.max(idx_not_nan[:, 1]))
     elif cols.strip() == 'all':
         colrange = (0, KEPLER_CHANNEL_SHAPE[1])
     else:
-        colrange = cols.split("..")
+        colrange = [int(c) for c in cols.split("..")]
+
+    if cut is not None:
+        cut = [int(c) for c in cut.split("..")]
 
     #from .video import make_video
     #make_video(mosaic_filenames, rowrange=rowrange, colrange=colrange)
     from .video import KeplerMosaicVideo
     kmv = KeplerMosaicVideo(mosaic_filenames, colrange=colrange, rowrange=rowrange)
-    kmv.export_frames()
+    kmv.export_frames(extension=ext, cut=cut)
 
 
 if __name__ == '__main__':
