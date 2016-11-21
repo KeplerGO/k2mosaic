e12094d55dd4bf34b35f38f4cd309135f7fdea52
Geert Barentsen <geert@barentsen.be>
2016-11-21 08:15:49 -0800

    Add test to see if we can run the subcommands

--- k2mosaic/tests/test_core.py
+++ k2mosaic/tests/test_core.py
@@ -1,7 +1,22 @@
 import pytest
 
+import k2mosaic
 from k2mosaic import ui
 
-def test_main():
+
+def test_commands():
+    """Check if the command-line tools can be run without crash."""
+    # If a command runs successfully it will raise a SystemExit at the end
     with pytest.raises(SystemExit):
         ui.k2mosaic()
+    # Also check the sub-commands:
+    with pytest.raises(SystemExit):
+        ui.mosaic()
+    with pytest.raises(SystemExit):
+        ui.movie()
+    with pytest.raises(SystemExit):
+        ui.tpflist()
+
+
+def test_has_version_variable():
+    k2mosaic.__version__
