6369910e5600ca200081bd992fa799b6ea1236c4
Ann Marie Cody <annmarie.cody@gmail.com>
2018-12-13 15:52:50 -0800

    added EXTNAME to the primary header, and made DATA_REL be undefined

--- k2mosaic/mosaic.py
+++ k2mosaic/mosaic.py
@@ -176,6 +176,8 @@ class KeplerChannelMosaic(object):
         # Override the defaults where necessary
         hdu.header['NEXTEND'] = 3
         hdu.header.cards['NEXTEND'].comment = 'number of standard extensions'
+        hdu.header['EXTNAME'] = 'PRIMARY'
+        hdu.header.cards['EXTNAME'].comment = 'name of extension'
         hdu.header['EXTVER'] = 1
         hdu.header.cards['EXTVER'].comment = 'extension version number (not format version)'
         hdu.header['ORIGIN'] = "NASA/Ames"
@@ -206,7 +208,7 @@ class KeplerChannelMosaic(object):
         hdu.header.cards['INSTRUME'].comment = 'detector type'
         hdu.header['FILTER'] = 'Kepler'
         hdu.header.cards['FILTER'].comment = 'photometric passband'
-        hdu.header['DATA_REL'] = 1
+        hdu.header['DATA_REL'] = UNDEFINED
         hdu.header.cards['DATA_REL'].comment = 'data release version number'
 
         hdu.header['ASTATE'] = UNDEFINED
